14caf4f4217fb1d39872e863d248d729574ae7a7
Roko Marusic <roko.marusic@fer.hr>
2021-03-06 18:11:18 +0100

    added textview and button and onclick listener

--- app/src/main/java/com/example/projekt1/MainActivity.kt
+++ app/src/main/java/com/example/projekt1/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.projekt1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    var showing: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showHide : Button = findViewById(R.id.btShowHide)
+        val hw : TextView = findViewById(R.id.tvHelloWorld)
+
+        showHide.setOnClickListener {
+            if(showing){
+                hw.visibility = View.INVISIBLE
+                showHide.text = "Show"
+            }else{
+                hw.visibility = View.VISIBLE
+                showHide.text = "Hide"
+            }
+            showing = !showing
+        }
     }
 }
